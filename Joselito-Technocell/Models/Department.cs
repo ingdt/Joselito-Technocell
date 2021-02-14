0c33de0e4e0479b929658ea532722d17b000761b
Elvin Antonio Burgos <praslay@hotmail.com>
2021-02-14 11:23:37 -0400

    DEPARTAMENT

---
+++ Joselito-Technocell/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Joselito_Technocell.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        public string Name { get; set; }
+
+        //[OneToMany(CascadeOperations = CascadeOperation.All)]
+        //public List<City> Cities { get; set; }
+
+        //[OneToMany(CascadeOperations = CascadeOperation.All)]
+        //public List<Customer> Customers { get; set; }
+
+        public override int GetHashCode()
+        {
+            return DepartmentId;
+        }
+    }
+
+}
