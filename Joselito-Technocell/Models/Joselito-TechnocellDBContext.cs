9a269c87dd18a137a87636cfcace4c6682eddbb7
Elvin Antonio Burgos <praslay@hotmail.com>
2021-02-14 10:58:20 -0400

    ejemplo1

---
+++ Joselito-Technocell/Models/Joselito-TechnocellDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Joselito_Technocell.Models
+{
+    public class Joselito_TechnocellDBContext : DbContext
+    {
+        public Joselito_TechnocellDBContext() : base("DefaultConnection")
+        {
+                
+        }
+    }
+}
