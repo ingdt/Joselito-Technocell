2a170778f55ccf56b6d68c0e7548efae8acad10d
Jean <lupyn_1@outlook.com>
2021-02-15 18:11:56 -0400

    error de DBcontext

--- Joselito-Technocell/Joselito-Technocell/Models/Joselito-TechnocellDbContext.cs
+++ Joselito-Technocell/Joselito-Technocell/Models/Joselito-TechnocellDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace Joselito_Technocell.Models
 {
-    public class Joselito_TechnocellDbContext
+    public class Joselito_TechnocellDbContext: DbContext
     {
         public Joselito_TechnocellDbContext() : base("DefaultConnection")
         {
