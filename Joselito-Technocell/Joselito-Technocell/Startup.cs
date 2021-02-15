faa48a5707048e830371e846fe60d4e46768e1e2
Elvin Antonio Burgos <praslay@hotmail.com>
2021-02-15 15:26:01 -0400

    1

---
+++ Joselito-Technocell/Joselito-Technocell/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Joselito_Technocell.Startup))]
+namespace Joselito_Technocell
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
