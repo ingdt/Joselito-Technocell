265e1a41a694ea5c750b26276e2ddf44f9f12c71
Elvin Antonio Burgos <praslay@hotmail.com>
2021-02-15 16:32:58 -0400

    Context

---
+++ Joselito-Technocell/Joselito-Technocell/Models/Joselito-TechnocellDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Joselito_Technocell.Models
+{
+    public class Joselito_TechnocellDbContext
+    {
+        public Joselito_TechnocellDbContext() :base ("")
+        {
+
+        }
+    }
+}
