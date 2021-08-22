4f2dd4252ba7769b72a0d9f3006c85737d7a0997
Elvin Antonio Burgos <praslay@hotmail.com>
2021-08-22 10:23:40 -0400

    Thema Lumen

--- Joselito-Technocell/Joselito-Technocell/App_Start/BundleConfig.cs
+++ Joselito-Technocell/Joselito-Technocell/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Joselito_Technocell
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
