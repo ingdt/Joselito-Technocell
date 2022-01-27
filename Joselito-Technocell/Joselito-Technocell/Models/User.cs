f7818e1f4d2c3b2221a4a9bbd6d57dcd1a50adb4
Xavier Berroa <xavilitoh@gmail.com>
2022-01-26 23:11:43 -0400

    validaciones 2.0

--- Joselito-Technocell/Joselito-Technocell/Models/User.cs
+++ Joselito-Technocell/Joselito-Technocell/Models/User.cs
@@ -22,7 +22,11 @@ namespace Joselito_Technocell.Models
         
         [DataType(DataType.PhoneNumber)]
         [Display(Name = "Teléfono")]
+        [RegularExpression("^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$", ErrorMessage = "Por favor introduzca un Número con el formato correcto ejemplo: 000-000-0000")]
         public override string PhoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Por favor introduzca una direccion de correo valida")]
+        public override string Email { get; set; }
 
         [Display(Name = "Nombre Completo")]
         public string FullName { get { return $"{this.FirstName} {this.LastName}"; } }
